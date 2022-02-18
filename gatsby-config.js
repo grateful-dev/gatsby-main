52f36e87bf3790e3aaef3a6fd579154fc6751efe
grateful-dev <49768627+grateful-dev@users.noreply.github.com>
2022-02-18 23:35:19 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,15 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `grateful.dev ~ strategic procrastination consulting`,
+    siteTitleAlt: `grateful.dev`,
+    siteHeadline: `grateful.dev`,
+    siteUrl: `https://grateful.dev`,
+    siteDescription: `data processing, generative arts, radio tek, alt cultures and society, internet culture and history`,
+//    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@zgratefuldev`,
   },
   plugins: [
     {
